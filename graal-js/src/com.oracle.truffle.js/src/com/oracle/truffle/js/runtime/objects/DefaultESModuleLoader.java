753fbca458386a97ae480305b872e7f165ff59b6
Jan Stola <jan.stola@oracle.com>
2026-02-09 13:12:41 +0100

    Avoiding NPE when importing the root directory.

--- graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/objects/DefaultESModuleLoader.java
+++ graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/objects/DefaultESModuleLoader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -517,6 +517,9 @@ public class DefaultESModuleLoader implements JSModuleLoader {
     }
 
     private static String findMimeTypeFromExtension(String moduleName) {
+        if (moduleName == null) {
+            return null;
+        }
         if (moduleName.endsWith(JavaScriptLanguage.JSON_SOURCE_NAME_SUFFIX)) {
             return JavaScriptLanguage.JSON_MIME_TYPE;
         }
